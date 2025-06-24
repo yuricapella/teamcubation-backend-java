b1f4738cd880a2cf376f609afa5fcdc7a0171b6d
yuricapella <yuricapelladossantos@gmail.com>
2025-06-24 18:26:05 -0300

    testando

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int idade = 0; idade <= 18; idade++) {
+            System.out.println("idade = " + idade);
+        }
+    }
+}
