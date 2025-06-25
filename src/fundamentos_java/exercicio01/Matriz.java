cdb48c02519d47abd2fc8dfc933e0d6b57eedfc1
yuricapella <yuricapelladossantos@gmail.com>
2025-06-25 09:07:21 -0300

    feat(fundamentos_java): adiciona exercicio 01, preenche matriz 3x3 e calcula valor total

--- src/fundamentos_java/exercicio01/Matriz.java
+++ src/fundamentos_java/exercicio01/Matriz.java
@@ -1,4 +1,20 @@
 package fundamentos_java.exercicio01;
+import java.util.Scanner;
+
 
 public class Matriz {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int resultado = 0;
+        int[][] matriz = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Digite os valores da matriz, linha " + (i + 1) + ", coluna " + (j + 1) + ":");
+                int valor = entrada.nextInt();
+                resultado += valor;
+                matriz[i][j] = valor;
+            }
+        }
+        System.out.println("Matriz resultado: " + resultado);
+    }
 }
